de661ddb0118fdf20965470d8e3dfd3cfb2f42d7
Luis Mesajil <lhmesajil@gmail.com>
2024-08-29 17:35:33 -0500

    fix(app.test): add afterAll to close server

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest')
-const app = require('../src/app')
+const { app, server } = require('../src/app')
+
 describe('GET /', () => {
+  afterAll(() => {
+    server.close()
+  })
+
   it('should return Hello, World!', async () => {
     const res = await request(app).get('/')
     expect(res.statusCode).toEqual(200)
